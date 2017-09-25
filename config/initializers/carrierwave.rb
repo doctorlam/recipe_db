46dd2e33a2156490008c414ff5abc9cb21753fdb
Chris <chris.lam.unt@gmail.com>
2017-09-25 16:44:34 -0500

    aws setup removing default lang

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,8 +15,8 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => '<your key goes here>',            # required
-    :aws_secret_access_key  => '<your secret key goes here>',     # required
+    :aws_access_key_id      => '',            # required
+    :aws_secret_access_key  => '',     # required
     :region                 => 'us-east-2'                        # optional, defaults to 'us-east-1'
   }
         config.storage = :fog
