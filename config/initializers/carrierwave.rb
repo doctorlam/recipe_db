421a42573bd761766c86352a261856b2530efbc7
Chris <chris.lam.unt@gmail.com>
2017-09-25 16:27:31 -0500

    new carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,33 +1,27 @@
-# config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    # Configuration for Amazon S3 should be made available through an Environment variable.
-    # For local installations, export the env variable through the shell OR
-    # if using Passenger, set an Apache environment variable.
-    #
-    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-    #
-    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
-
-    # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
-  }
-
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
+  
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
   end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
-
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+  
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => '<your key goes here>',            # required
+    :aws_secret_access_key  => '<your secret key goes here>',     # required
+    :region                 => 'es-east-2'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = '<bucket name goes here>'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
