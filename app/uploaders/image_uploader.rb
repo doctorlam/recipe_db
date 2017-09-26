4721a0a42dbfb411560b7839f9cc73fd63cd72c5
Chris <chris.lam.unt@gmail.com>
2017-09-26 07:53:04 -0500

    Adding carrierwave uploads

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,8 +2,4 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 storage :fog
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,14 @@
 CarrierWave.configure do |config|
-  
-  # Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :image
-    end
-  end
-  
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-  
-  
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    :provider               => 'AWS',
-      :aws_access_key_id      => "YOUR AMAZON ACCESS KEY",
-      :aws_secret_access_key  => "YOUR AMAZON SECRET KEY",
-    :region                 => 'us-east-2'                        # optional, defaults to 'us-east-1'
+    provider:              'AWS',                        # required
+    aws_access_key_id:  ENV ["AWS_ACCESS_KEY_ID"],                        # required
+    aws_secret_access_key: ENV ["AWS_SECRET_ACCESS_KEY",                        # required
+    region:                'us-east-2',                  # optional, defaults to 'us-east-1'
+    host:                  's3.example.com',             # optional, defaults to nil
+    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-        config.storage = :fog
-
-  config.fog_directory  = 'lam-fall'               # required
-  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  #config.fog_public     = false                                  # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_directory  = 'lam-fall'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
-  end
-  end
